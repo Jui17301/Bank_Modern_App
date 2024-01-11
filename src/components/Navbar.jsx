f7782847f7c29c9cf8ae7677dabd743bd0338390
Jui <cse_1912020087@gmail.com>
2024-01-11 23:07:05 +0600

    Navbar-updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,8 +9,10 @@ const Navbar = () => {
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
       {
         navLinks.map((nav,index)=>{
-          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index===navLinks.length-1?'mr-0':'mr-10'} text-white`}>
-          <a href="{`#${nav.id}`}">
+          <li 
+          key={nav.id} 
+          className={`font-poppins font-normal cursor-pointer text-[16px] ${index===navLinks.length-1?'mr-0':'mr-10'}`}>
+          <a href={`#${nav.id}`}>
             {nav.title}
           </a>
           
@@ -21,15 +23,29 @@ const Navbar = () => {
 
       <div className='sm:hidden flex 
        flex-1 justify-end items-center'>
-<img src={toggle? close: menu} alt="menu" className='w-[28px] h-[28px] object-contain' onClick={()=>setToggle((prev)=> !prev)} />
-<div className={`${toggle? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar `}>
+<img 
+  src={toggle? close: menu} 
+  alt="menu" 
+  className='w-[28px] h-[28px] object-contain' 
+  onClick={()=>setToggle(!toggle)} />
+ 
+ <div 
+    className={`
+    ${toggle? 'flex' : 'hidden'} 
+    p-6 bg-black-gradient absolute top-20 right-0 
+    mx-4 my-2 min-w-[140px] rounded-xl sidebar `}>
 
-<ul className='list-none flex 
+<ul 
+  className='list-none flex 
  flex-col justify-end items-center flex-1'>
       {
         navLinks.map((nav,index)=>{
-          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index===navLinks.length-1?'mr-0':'mb-4'} text-white`}>
-          <a href="{`#${nav.id}`}">
+          <li 
+          key={nav.id} 
+          className={`font-poppins font-normal 
+            cursor-pointer text-[16px] 
+             ${index===navLinks.length-1?'mr-0':'mb-4'} `}>
+          <a href={`#${nav.id}`}>
             {nav.title}
           </a>
           
